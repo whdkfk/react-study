d645b9ef7004d4203bfbb40364b60fca74c3e259
whdkfk <joara080827@gmail.com>
2024-09-17 23:39:51 +0900

    Handling Events

--- react/src/component/Hello.js
+++ react/src/component/Hello.js
@@ -1,20 +1,28 @@
-import World from "./World";
-import styles from"./Hello.module.css";
-
 export default function Hello(){
+    function showName(){
+        console.log("ara");
+    }
+    function showAge(age){
+        console.log(age);
+    }
+    function showText(txt){
+        console.log(txt);
+    }
     return (
         <div>
-            <h1 
-                style={{
-                    color: '#f0f',
-                    borderRight: '2px solid #000',
-                    marginBottom: '50px',
-                    opacity: 1,
+            <h1>Hello</h1>
+            <button onClick={showName}>Show name</button>
+            <button
+                onClick={() => {
+                    showAge(17)
                 }}
             >
-                Hello
-            </h1>
-            <div className={styles.box}>Hello</div>
+                Show age
+            </button>
+            <input type="text" onChange={e => {
+                const txt = e.target.value;
+                showText(txt);
+            }}/>
         </div>
     );
 }
