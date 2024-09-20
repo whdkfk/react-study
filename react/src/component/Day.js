e44d31af2e84bc7e5166b3003977b898bd18baee
whdkfk <joara080827@gmail.com>
2024-09-20 21:42:50 +0900

    useEffect, fetch()로 API 호출

--- react/src/component/Day.js
+++ react/src/component/Day.js
@@ -1,17 +1,28 @@
-import dummy from "../db/data.json";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
+import { useEffect } from "react";
 
 export default function Day(){
     const { day } = useParams();
-    const wordList = dummy.words.filter(word => word.day === Number(day));
+    const [words, setWords] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            setWords(data);
+        });
+    }, [])
     
     return (
         <>
         <h2>Day {day}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word => (
+                    {words.map(word => (
                         <Word word={word} key={word.id} />
                     ))}
                 </tbody>
